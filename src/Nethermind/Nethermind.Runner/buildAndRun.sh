f31491c0cad27a4db84e6b22c5b6f64338e9ed58
lyfsn <dev.wangyu@proton.me>
2024-03-14 17:35:13 +0800

    up

---
+++ src/Nethermind/Nethermind.Runner/buildAndRun.sh
@@ -0,0 +1,17 @@
+dotnet run -c release -- \
+  --log=INFO \
+  --datadir=./execution-data \
+  --Init.ChainSpecPath=./chainspec.json \
+  --Init.WebSocketsEnabled=true \
+  --JsonRpc.Enabled=true \
+  --JsonRpc.EnabledModules=net,eth,consensus,subscribe,web3,admin \
+  --JsonRpc.Host=0.0.0.0 \
+  --JsonRpc.Port=8545 \
+  --JsonRpc.WebSocketsPort=8546 \
+  --JsonRpc.EngineHost=0.0.0.0 \
+  --JsonRpc.EnginePort=8551 \
+  --Network.ExternalIp=127.0.0.1 \
+  --Network.DiscoveryPort=30303 \
+  --Network.P2PPort=30303 \
+  --Metrics.Enabled=true \
+  --Metrics.ExposePort=9001 \
